dd36d0a910ef0cfed8e18ed0fe21b3434719ab02
Dushyant Singh <dushyant.24bcon2017@jecrcu.edu.in>
2026-06-05 20:09:33 +0530

    added supabase

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,15 @@
 import { PrismaClient } from "../app/generated/prisma/client"; 
 import { PrismaPg } from "@prisma/adapter-pg"; 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient; 
-}; 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL, 
-}); 
-const prisma =
-  globalForPrisma.prisma ||
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
+export const prisma =
+  globalForPrisma.prisma ??
   new PrismaClient({
-    adapter, 
-  }); 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
-export default prisma; 
+    adapter: new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    }),
+  });
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
